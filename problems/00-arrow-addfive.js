0504c311e8215e569ea059982c6fd049cc93363d
nelson <panamenonelson@gmail.com>
2022-06-11 09:14:12 -0700

    re-solving problems with partner

--- problems/00-arrow-addfive.js
+++ problems/00-arrow-addfive.js
@@ -14,15 +14,9 @@ let result3 = addFive(37); // returns 42
 
 
 ***********************************************************************/
+let addFive = (num) => num + 5;
 
-let addFive = (n) => {
-  let added = (n + 5);
-  return added;
-};
 
-let result1 = addFive(0); // returns 5
-let result2 = addFive(10); // returns 15
-let result3 = addFive(37); // returns 42
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
